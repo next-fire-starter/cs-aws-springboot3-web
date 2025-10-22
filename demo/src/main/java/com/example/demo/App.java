717c8c334941a6826ea7bb0c2b4a42844df20fd9
next-fire-starter <next.fire.starter@gmail.com>
2025-10-22 14:44:13 +0000

    add lambda

--- demo/src/main/java/com/example/demo/App.java
+++ demo/src/main/java/com/example/demo/App.java
@@ -1,19 +1,42 @@
 package com.example.demo;
 
+import com.example.demo.controller.SampleController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+@Import({ SampleController.class })
+public class App  {
+
+    @Value("${logging.level.root:OFF}")
+    String message = "";
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
-    }
+    // @Override
+    // protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    //     return application.sources(App.class);
+    // }
 }
